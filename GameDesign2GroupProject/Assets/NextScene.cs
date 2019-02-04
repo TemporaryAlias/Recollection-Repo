f7fcb5bcfdca64224e4227722909ebb4f97c36ad
Eibho <eibhyor@gmail.com>
2019-02-04 14:20:47 +0000

    Done

---
+++ GameDesign2GroupProject/Assets/NextScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NextScene : MonoBehaviour
+{
+
+    [SerializeField]
+    public float delayTime = 10f;
+    public int loadThisScene = 2;
+
+    private float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if(timeElapsed > delayTime)
+        {
+            SceneManager.LoadScene(loadThisScene);
+        }
+    }
+}
