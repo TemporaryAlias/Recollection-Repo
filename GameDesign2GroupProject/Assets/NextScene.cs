ab53738ad4f80122e142d29d4aeaa4ce8333e06a
TemporaryAlias <38929684+TemporaryAlias@users.noreply.github.com>
2019-02-05 14:36:43 +0000

    Fixed some bugs and created a Windows build

--- GameDesign2GroupProject/Assets/NextScene.cs
+++ GameDesign2GroupProject/Assets/NextScene.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class NextScene : MonoBehaviour
 {
@@ -13,10 +13,20 @@ public class NextScene : MonoBehaviour
 
     private float timeElapsed;
 
+    public Image fadeOutImage;
+
+    Color fadeColour;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fadeColour = new Color(0, 0, 0, 0);
+
+        fadeOutImage.color = fadeColour;
+
+        fadeOutImage.gameObject.SetActive(false);
+
+        FadeIn();
     }
 
     // Update is called once per frame
@@ -26,7 +36,46 @@ public class NextScene : MonoBehaviour
 
         if(timeElapsed > delayTime)
         {
-            SceneManager.LoadScene(loadThisScene);
+
+            FadeToScene(2);
         }
     }
+
+    public void StartFadeIn() {
+        StartCoroutine("FadeIn");
+    }
+
+    public void FadeToScene(int nextSceneIndex) {
+        StartCoroutine("FadeOut", nextSceneIndex);
+    }
+
+    IEnumerator FadeOut(int nextSceneIndex) {
+        fadeOutImage.gameObject.SetActive(true);
+
+        while (fadeColour.a < 1) {
+            fadeColour.a += 0.02f;
+
+            fadeOutImage.color = fadeColour;
+
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        LevelManager._instance.ChangeSceneByIndex(nextSceneIndex);
+    }
+
+    IEnumerator FadeIn() {
+        fadeOutImage.gameObject.SetActive(true);
+        fadeColour.a = 1;
+
+        while (fadeColour.a > 0) {
+            fadeColour.a -= 0.01f;
+
+            fadeOutImage.color = fadeColour;
+
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        fadeOutImage.gameObject.SetActive(false);
+    }
+
 }
